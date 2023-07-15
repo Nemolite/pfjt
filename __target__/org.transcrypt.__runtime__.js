96ef3135f165e384337bf37b849175be0734aba3
Nemolite <g25092011@mail.ru>
2023-07-15 19:52:23 +0300

    JQuery/console.log

--- __target__/org.transcrypt.__runtime__.js
+++ __target__/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2023-07-15 19:17:30
+// Transcrypt'ed from Python, 2023-07-15 19:51:32
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
---
+++ external_jq.py
@@ -0,0 +1,9 @@
+# __pragma__ ('alias', 'jq', '$')
+
+def set_click():
+    def add_item():
+        jq("ol").append("<li>List item</li>")
+
+    jq("#append_btn").click(add_item)
+
+jq(document).ready(set_click)
